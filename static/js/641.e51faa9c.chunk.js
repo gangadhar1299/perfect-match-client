4fab9f72deb887c791299e6bd0b980c33e5e2489
Gangadhar N <gagan@Gangadhars-MacBook-Air.local>
2022-01-26 14:01:45 +0530

    Updates

---
+++ static/js/641.e51faa9c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[641],{641:function(e,n,t){t.r(n);var c=t(184);n.default=function(){return(0,c.jsx)("div",{children:"Authenticated"})}}}]);
+//# sourceMappingURL=641.e51faa9c.chunk.js.map
